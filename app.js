15ada210fb13b6d350bc22c8f5a8d75b11418da2
Mohd Imran <mohd78696321@gmail.com>
2021-09-28 09:38:19 +0530

    feat: updated logic

--- app.js
+++ app.js
@@ -4,36 +4,38 @@ const currentPrice = document.querySelector("#current-price");
 const checkBtn = document.querySelector("#check-btn");
 const output = document.querySelector("#output");
 
-function calculateProfitAndLoss(initial, quantity, current) {
-  if (initial > current) {
-    var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
-
-    showOutput(
-      `Hey! The loss is ${loss} and the loss percentage is ${lossPercentage}%`
-    );
-  } else if (current > initial) {
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
-
-    showOutput(
-      `Hey! The profit is ${profit} and the profit percentage is ${profitPercentage}%`
-    );
-  } else {
-    showOutput("Please Input Values");
-  }
-}
-
 function submitHandler() {
   var ip = Number(initialPrice.value);
   var qty = Number(stocksQuantity.value);
   var curr = Number(currentPrice.value);
+  if (!ip || !qty || !curr) {
+    output.innerText = "Enter all the inputs";
+  } else if (ip < 1 || qty < 1 || curr < 1) {
+    output.innerText = "Enter the positive value greater than zero";
+  } else {
+    calculateProfitAndLoss(ip, qty, curr);
+  }
+}
 
-  calculateProfitAndLoss(ip, qty, curr);
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial < current) {
+    var profit = ((current - initial) * quantity).toFixed(2);
+    var profitPercentage = ((profit / initial) * 100).toFixed(2);
+
+    showOutput(
+      `Hey the profit is ${profit} and percent is ${profitPercentage}%`
+    );
+  } else if (initial > current) {
+    var loss = ((initial - current) * quantity).toFixed(2);
+    var lossPercentage = ((loss / initial) * 100).toFixed(2);
+    showOutput(`Hey the loss is ${loss} and percent is ${lossPercentage}%`);
+  } else {
+    showOutput("Try Your Luck Next Time!");
+  }
 }
 
 function showOutput(message) {
-  output.innerText = message;
+  output.innerHTML = message;
 }
 
 checkBtn.addEventListener("click", submitHandler);
